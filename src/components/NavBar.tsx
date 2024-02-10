8b9cf9cf8ce8e80512d9f09dd5fd80194b98a202
rawidaniel <rawidaniel96@gmail.com>
2024-02-10 18:33:46 +0300

    feat: change the logo name

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,11 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logog from "../assets/logo.webp";
+import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 function NavBar() {
   return (
     <HStack justifyContent="space-between" padding="10px">
-      <Image src={logog} boxSize="60px" />
+      <Image src={logo} boxSize="60px" />
       <ColorModeSwitch />
     </HStack>
   );
