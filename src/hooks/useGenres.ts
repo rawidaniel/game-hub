3204e9f8846ee5d84b3adb31d4bda1d823538621
rawidaniel <rawidaniel96@gmail.com>
2024-02-28 12:26:50 +0300

    refactor: simplify time calculations

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
+
 import genres from "../data/genres";
 import ApiClient from "../services/api-client";
 
@@ -13,7 +15,7 @@ const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000,
+    staleTime: ms("24h"),
     initialData: { count: genres.length, results: genres, next: null },
   });
 
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
 import ApiClient from "../services/api-client";
 import platform from "../data/platforms";
 
@@ -13,7 +14,7 @@ const usePlatforms = () =>
   useQuery({
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000,
+    staleTime: ms("24h"),
     initialData: { count: platform.length, results: platform, next: null },
   });
 
