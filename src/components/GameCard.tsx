7b67b95fa022059dbb4b4ac871e1696453956194
rawidaniel <rawidaniel96@gmail.com>
2024-03-16 10:34:52 +0300

    feat: fetch game details

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,6 +4,7 @@ import PlatformIconLists from "./PlatformIconLists";
 import CriticScore from "./CriticScore";
 import getCroppedIMage from "../services/image-url";
 import Emoji from "./Emoji";
+import { Link } from "react-router-dom";
 
 interface GameCardProps {
   game: Game;
@@ -20,7 +21,8 @@ function GameCard({ game }: GameCardProps) {
           <CriticScore score={game.metacritic} />
         </HStack>
         <Heading fontSize="2xl">
-          {game.name} <Emoji rating={game.rating_top} />
+          <Link to={`games/${game.slug}`}>{game.name} </Link>
+          <Emoji rating={game.rating_top} />
         </Heading>
       </CardBody>
     </Card>
