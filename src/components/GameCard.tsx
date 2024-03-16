eb220743a02c882675521c48ca303ccf4a02cb4e
rawidaniel <rawidaniel96@gmail.com>
2024-03-16 10:58:15 +0300

    refactor: move entity interfaces

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,5 @@
 import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
-import { Game } from "../hooks/useGames";
+import { Game } from "../entities/Game";
 import PlatformIconLists from "./PlatformIconLists";
 import CriticScore from "./CriticScore";
 import getCroppedIMage from "../services/image-url";
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -3,14 +3,9 @@ import ms from "ms";
 
 import genres from "../data/genres";
 import ApiClient from "../services/api-client";
+import { Genre } from "../entities/Genre";
 
 const apiClient = new ApiClient<Genre>("/genres");
-export interface Genre {
-  id: number;
-  name: string;
-  image_background: string;
-}
-
 const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
